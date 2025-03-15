db5f0ba79297f8142efec464406536ad8c13062c
22520565 <22520565@gm.uit.edu.vn>
2025-03-16 01:17:06 +0700

    Config `GameManager`.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,8 +3,41 @@
 namespace Game
 {
     using UnityEngine;
+    using UnityEngine.SceneManagement;
 
-    public class GameManager : MonoBehaviour
+    public class GameManager : SingletonMonoBehaviour<GameManager>
     {
+        public string MainMenuSceneName { get; } = "MainMenu";
+
+        public string GameplaySceneName { get; } = "Gameplay";
+
+        public string HighScorePrefKey { get; } = "HighScore";
+
+        public int CurrentScore { get; private set; } = 0;
+
+        public int HighScore
+        {
+            get
+            {
+                return PlayerPrefs.GetInt(this.HighScorePrefKey, 0);
+            }
+
+            set
+            {
+                PlayerPrefs.SetInt(this.HighScorePrefKey, value);
+            }
+        }
+
+        protected override GameManager LocalInstance => this;
+
+        public void GoToMainMenu()
+        {
+            SceneManager.LoadScene(this.MainMenuSceneName);
+        }
+
+        public void GoToGameplay()
+        {
+            SceneManager.LoadScene(this.GameplaySceneName);
+        }
     }
 }
