89ab4989800b9d82ffbde9d0fe7f7a08eb183287
22520565 <22520565@gm.uit.edu.vn>
2025-03-16 22:41:19 +0700

    Config build profile.

--- Assets/Scripts/Drawers/ReadOnlyInInspectorDrawer.cs
+++ Assets/Scripts/Drawers/ReadOnlyInInspectorDrawer.cs
@@ -1,17 +1,22 @@
+#if UNITY_EDITOR
+
 #nullable enable
 
-namespace Game;
-
-using UnityEditor;
-using UnityEngine;
-
-[CustomPropertyDrawer(typeof(ReadOnlyInInspectorAttribute))]
-public sealed class ReadOnlyInInspectorDrawer : PropertyDrawer
+namespace Game
 {
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    using UnityEditor;
+    using UnityEngine;
+
+    [CustomPropertyDrawer(typeof(ReadOnlyInInspectorAttribute))]
+    public sealed class ReadOnlyInInspectorDrawer : PropertyDrawer
     {
-        GUI.enabled = false;
-        EditorGUI.PropertyField(position, property, label, true);
-        GUI.enabled = true;
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            GUI.enabled = false;
+            EditorGUI.PropertyField(position, property, label, true);
+            GUI.enabled = true;
+        }
     }
 }
+
+#endif
